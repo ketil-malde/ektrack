6f433d16cba996b611ead225a0324351d364aa20
Ketil Malde <ketil@malde.org>
2026-01-12 12:59:32 +0100

    Remove old dev code from prominence, include imports in main

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
+from prominence import prominence
+import track
+
 def main():
     print("Hello from ektrack2!")
-
+    # 1. read NetCDF file
+    # 2. calculate prominences
+    # 3. run tracking
 
 if __name__ == "__main__":
     main()
