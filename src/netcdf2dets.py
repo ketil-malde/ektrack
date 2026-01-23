9566c15ecd04452d19f18730a3528f81bc79a1b3
Ketil Malde <ketil@malde.org>
2026-01-23 10:38:24 +0100

    Create Detections, incorporate type and frequency as attributes.

--- src/netcdf2dets.py
+++ src/netcdf2dets.py
@@ -4,6 +4,8 @@ import numpy as np
 from sys import argv
 
 from prominence import prominence
+from track import Detection
+
 
 def readnetcdf(ncfile):
     """
@@ -11,25 +13,29 @@ def readnetcdf(ncfile):
     Returns an xarray with (pulse-compressed for bb) backscatter and angles.
     """
     nc_data = Dataset(ncfile)
+    freqs = list(nc_data.variables['frequency'][:])
     res = {}
     for g in nc_data.groups.keys():
         if g == 'Environment': continue
         # Each g corresponds to one frequency
+        freq = freqs.pop(0)
         d = xr.open_dataset(ncfile, engine='netcdf4', group=g)
         wbtlabel = d.attrs['channel_id']
         if 'pulse_compressed_re' in d:
             # FM data - calc mean magnitude across sectors (quadrants)
             backscatter = abs(d['pulse_compressed_re'] + d['pulse_compressed_im'] * 1j).mean(dim='sector')
+            atype = 'FM'
         elif 'sv' in d:
             # CW data (no pulse comression, keep sv (should be TS, no?))
             backscatter = d['sv']
+            atype = 'CV'
         else:
             raise RunTimeError(f'Neither FM nor CW data in {ncfile}/{wbtlabel}?')  # noqa - flake doesn't know about RTE
 
         theta = d['angle_alongship']
         phi = d['angle_athwartship']
 
-        res[g] = xr.Dataset({'backscatter': backscatter, 'theta': theta, 'phi': phi}).assign_attrs(wbtlabel=wbtlabel)
+        res[g] = xr.Dataset({'backscatter': backscatter, 'theta': theta, 'phi': phi}).assign_attrs(wbtlabel=wbtlabel, pulsetype=atype, frequency=freq)
 
     return res
 
@@ -60,11 +66,16 @@ def detections(pchannels):
                         ptm = gchan['ping_time'][ping].item()
                         theta = gchan['theta'][ping][i].item()
                         phi = gchan['phi'][ping][i].item()
-                        ds.append(f"time={ptm}\tR={rng:.2f}\ttheta={theta:.2f}\tphi={phi:.2f}\tval={val:.2f}")
+                        ds.append(Detection(pingno=ping, time=ptm, freq=0, range=rng, theta=theta, phi=phi, rank=0))
+                        # ds.append(f"time={ptm}\tR={rng:.2f}\ttheta={theta:.2f}\tphi={phi:.2f}\tval={val:.2f}")
             dss.append(ds)
         ret[g] = dss
     return ret
 
+
+# ../data/D20230803-T230004.nc <- salmon plus seabed?
+# Generate the NetCDF by running raw2pc.py from CRIMAC-FM-testdatapaper on D20230803-T230004.raw
+# ..except that generates four outputs: pc_{1..4}, none of them matching this file.
 
 if __name__ == '__main__':
     # read args[1]
@@ -75,5 +86,5 @@ if __name__ == '__main__':
     print(ch)
     # compute the detections
     for g, ds in detections(ch).items():
-        print(g, ch[g].wbtlabel, len(ds[7]))
+        print(f'{g}: "{ch[g].wbtlabel}" {int(ch[g].frequency)}kHz, type={ch[g].pulsetype} len={len(ds[7])}')
         for s in ds[7]: print(s)
