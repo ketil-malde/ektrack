b600a36bb758a4e03e25450882612124a720e63e
Ketil Malde <ketil@malde.org>
2026-02-03 14:55:11 +0100

    Working main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,40 @@
 from netcdf2dets import readnetcdf, calc_prom_arrays, detections
-import track
+from track import track1, link_det
 
-def main():
+import sys
+
+def main(infile, pings):
     print("Hello from ektrack2!")
     # 1. read NetCDF file
+    ch = readnetcdf(infile)
+
     # 2. calculate prominences
-    # 3. run tracking
+    calc_prom_arrays(ch)
+
+    # 3. detections
+
+    # 4. run tracking
+    tracks = []
+    for p in pings:
+        dets = detections(ch, p, minprom=2.0, minrng=6.0, maxrng=8.0)
+        acc = None
+        for g, ds in dets.items():
+            ds = [[d] for d in ds]
+            if not acc:
+                acc = ds
+            else:
+                acc = link_det(acc, ds)
+        tracks = track1(tracks, acc)
+
+    return tracks
+
 
 if __name__ == "__main__":
-    main()
+    ts = main(sys.argv[1], range(100, 110))
+    for t in ts:
+        print('Track:')
+        for d in t.detections:
+            for e in d:
+                print(e)
+            print()
+        print()
