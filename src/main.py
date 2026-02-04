159359af3daa62bf4ac791d57b86f4437341a92e
Ketil Malde <ketil@malde.org>
2026-02-04 13:50:37 +0100

    Add plotting code to main

--- src/main.py
+++ src/main.py
@@ -1,19 +1,38 @@
 from netcdf2dets import readnetcdf, calc_prom_arrays, detections
 from track import track1, link_det
+from matplotlib import pyplot as plt
+import matplotlib.colors as mcolors
+
+PLOT = False
 
 import sys
 
 def main(infile, pings):
     # 1. read NetCDF file
+    print('Reading netCDF file: ' + infile)
     ch = readnetcdf(infile)
 
-    # 2. calculate prominences
+    # 2. calculate prominences - updates its parameter (ch)
     calc_prom_arrays(ch)
 
-    # 3. run tracking
+    if PLOT:
+      for g, x in ch.items():
+        fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(10, 5))
+        b = x['backscatter']
+        log_norm = mcolors.LogNorm(vmin=b.min().item(), vmax=b.max().item())
+        b.T.plot.imshow(ax=ax1, norm=log_norm)
+        ax1.set_title("Backscatter")
+        print(x['prominence'].max(), x['prominence'].min())  # prominence can be super-negative, why?
+        x['prominence'].T.plot.imshow(ax=ax2, cmap='Greys')
+        ax2.set_title("Prominence")
+        fig.suptitle(x.wbtlabel)
+        plt.tight_layout()
+        plt.show()
+
+    # 3. run tracking, iterating over pings
     tracks = []
     for p in pings:
-        dets = detections(ch, p, minprom=2.0, minrng=6.0, maxrng=8.0)
+        dets = detections(ch, p, minprom=1.0, minrng=6.0, maxrng=8.0)
         acc = None
         for g, ds in dets.items():
             ds = [[d] for d in ds]
