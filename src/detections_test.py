d97f63ebbc4b806ff124a0a7f3013bccac2f1842
Ketil Malde <ketil@malde.org>
2026-04-07 12:40:13 +0200

    Add test case for detection statistics

---
+++ src/detections_test.py
@@ -0,0 +1,25 @@
+# Test detections with various parameters.
+# Count singleton detections vs number of detections
+# Many detections, few singletons = good
+
+from netcdf2dets import load, detections
+from detections import cluster_det
+import sys
+
+if __name__ == '__main__':
+    print('Reading netCDF file: ' + sys.argv[1])
+    ds = load(sys.argv[1])
+    # Run tests/benchmark across parameters (randomsearch?)
+
+    # todo: vary angle_sigma and range_sigma
+    for prom in [1.0, 1.5, 2.0, 2.5]:
+        dsi = detections(ds, p=100, minrng=5, maxrng=50, minprom=prom)
+        for f, dets in dsi.items():
+            print(prom, f, len(dets))
+        print()
+
+        dcl = cluster_det(dsi)
+        # count number of detections and number of singletons
+        print('prom:', prom, 'clusters:', len(dcl), end='')
+        for ln in range(1, 6): print(f'  size{ln}: {len([d for d in dcl if len(d) == ln])}', end='')
+        print()
